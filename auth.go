0c661066d6537c2572ea2908d79053ebf0a8a72f
Nastya Polienko <polienkonastya2004@gmail.com>
2020-11-17 08:24:35 +0200

    jwt and rest api

---
+++ auth.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+)
+
+type key int
+
+const (
+	props key = iota
+)
+
+// Create the JWT key used to create the signature
+var jwtKey = []byte("g78rw9etuiwletru9w87g")
+
+// Credentials is the struct for holding email/pass/id combination
+type Credentials struct {
+	ID    int    `json:"uid"`
+	Pass  string `json:"pass"`
+	Email string `json:"email"`
+}
+
+// Claims is a part of token
+type Claims struct {
+	ID     int      `json:"uid"`
+	Email  string   `json:"email"`
+	jwt.StandardClaims
+}
+
+// AddAuthHandlers for main function handler
+func AddAuthHandlers(r *mux.Router) {
+	r.HandleFunc("/login/", LoginHandler).Methods("POST")
+}
+
+// LoginHandler Create the Signin handler
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var credsRequest, credsDB Credentials
+	var user User
+	// Get the JSON body and decode into credentials
+	err := json.NewDecoder(r.Body).Decode(&credsRequest)
+	if err != nil {
+		// If the structure of the body is wrong, return an HTTP error
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//only one tenant per uzer
+	err = db.QueryRow(`SELECT uid, email
+						FROM users
+						WHERE email=? AND pass=?`, credsRequest.Email, credsRequest.Pass).Scan(&credsDB.ID, &credsDB.Email)
+
+	if err != nil || credsDB.ID == 0 {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Declare the expiration time of the token
+	// here, we have kept it as 5 minutes
+	expirationTime := time.Now().Add(100 * time.Hour)
+	// Create the JWT claims, which includes the username and expiry time
+	var userList []User
+	userList = append(userList, user)
+
+	claims := &Claims{
+		Email:  credsRequest.Email,
+		ID:     credsDB.ID,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
+	b, err := json.Marshal(struct {
+		Token string `json:"token"`
+	}{
+		tokenString,
+	})
+	fmt.Fprintf(w, string(b))
+
+}
+
+// Middleware is a wrapper of handlers that needs to perform authentication
+func Middleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+		if len(authHeader) != 2 {
+			fmt.Println("Malformed token")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Malformed Token"))
+		} else {
+			jwtToken := authHeader[1]
+
+			claims := &Claims{}
+			tkn, err := jwt.ParseWithClaims(jwtToken, claims, func(token *jwt.Token) (interface{}, error) {
+				return jwtKey, nil
+			})
+			if err != nil {
+				if err == jwt.ErrSignatureInvalid {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if !tkn.Valid {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), props, claims)
+			// Access context values in handlers like this
+			// claims, _ := r.Context().Value("props").(*Claims)
+			// fmt.Println(claims)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+	})
+}
---
+++ main.go
@@ -0,0 +1,263 @@
+package main
+
+import(
+	"fmt"
+	"database/sql" 
+	"encoding/json"
+	"log"
+	"net/http"
+	_"github.com/go-sql-driver/mysql" 
+	"github.com/gorilla/mux"
+	"io/ioutil"
+)
+
+const(
+	CONN_PORT = "8080"
+	DRIVER_NAME = "mysql"
+	DATA_SOURCE_NAME = "root:1111@/library"
+	ADMIN_USER = "admin"
+	ADMIN_PASSWORD = "admin"
+	CLAIM_ISSUER = "Packt"
+	CLAIM_EXPIRY_IN_HOURS = 24
+)
+var db *sql.DB
+var connectionError error
+func init(){
+	db, connectionError = sql.Open(DRIVER_NAME, DATA_SOURCE_NAME)
+	if connectionError != nil{
+		log.Fatal("error connecting to database :: ", connectionError)
+	}
+}
+type Book struct{
+	Id int `json:"bid"`
+	Name string `json:"bookname"`
+	Year string `json:"year"`
+	User int `json:"uid"`
+	Status string `json:"status"`
+}
+
+type User struct{
+	Uid int `json:"uid"`
+	Email string `json:"email"`
+	Pass string `json:"pass"`
+}
+var user User
+
+func getBooks(w http.ResponseWriter, r *http.Request){
+	log.Print("reading records from database")
+	rows, err := db.Query("SELECT bid, bookname, year, uid, status FROM books")
+	if err != nil{
+		log.Print("error occurred while executing select query :: ",err)
+		return
+	}
+	books := []Book{}
+	for rows.Next(){
+		var bid int
+		var bookname string
+		var year string
+		var uid int
+		var process string
+		err = rows.Scan(&bid, &bookname, &year, &uid, &process)
+		book := Book{Id: bid, Name: bookname, Year: year, User: uid, Status: process}
+		books = append(books, book)
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	log.Print("reading a record from database")
+	result, err := db.Query("SELECT bid, bookname, year FROM books WHERE bid = ?", params["Id"])
+	if err != nil {
+	  panic(err.Error())
+	}
+	defer result.Close()
+	var book Book
+	for result.Next() {
+	  err := result.Scan(&book.Id, &book.Name, &book.Year)
+	  if err != nil {
+		panic(err.Error())
+	  }
+	}
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE books SET bookname = ?, year = ? WHERE bid = ?")
+	if err != nil {
+	  panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+	  panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newName := keyVal["bookname"]
+	newYear := keyVal["year"]
+	_, err = stmt.Exec(newName, newYear, params["Id"])
+	if err != nil {
+	  panic(err.Error())
+	}
+	log.Print("The book was updated")
+	fmt.Fprintf(w, "Book with Id = %s was updated", params["Id"])
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	stmt, err := db.Prepare("INSERT INTO books(bookname, year, uid) VALUES(?,?,?)")
+	if err != nil {
+	  panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+	  panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	
+	json.Unmarshal(body, &keyVal)
+	bookname := keyVal["bookname"]
+	year := keyVal["year"]
+	uid := keyVal["uid"]
+	_, err = stmt.Exec(bookname, year, uid)
+	if err != nil {
+	  panic(err.Error())
+	}
+	fmt.Fprintf(w, "New post was created")
+	log.Print("New post was created")
+  }
+
+  func deleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM books WHERE bid = ?")
+	if err != nil {
+	  panic(err.Error())
+	}
+	_, err = stmt.Exec(params["Id"])
+    if err != nil {
+	  panic(err.Error())
+	}
+  fmt.Fprintf(w, "Book with Id = %s was deleted", params["Id"])
+  }
+
+  func getUsBooks(w http.ResponseWriter, r *http.Request) {
+	log.Print("reading records from database")
+	params := mux.Vars(r)
+	rows, err := db.Query("SELECT bid, bookname, year FROM books WHERE uid = ?", params["uid"])
+	if err != nil{
+		log.Print("error occurred while executing select query :: ",err)
+		return
+	}
+	books := []Book{}
+	for rows.Next(){
+		var bid int
+		var bookname string
+		var year string
+		err = rows.Scan(&bid, &bookname, &year)
+		book := Book{Id: bid, Name: bookname, Year: year}
+		books = append(books, book)
+	}
+	json.NewEncoder(w).Encode(books)
+}
+  
+
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM users WHERE Uid = ?")
+	if err != nil {
+	  panic(err.Error())
+	}
+	_, err = stmt.Exec(params["Uid"])
+   if err != nil {
+	  panic(err.Error())
+	}
+  fmt.Fprintf(w, "User with uid = %s was deleted", params["Uid"])
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	stmt, err := db.Prepare("INSERT INTO users(email, pass) VALUES(?,?)")
+	if err != nil {
+	  panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+	  panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	
+	json.Unmarshal(body, &keyVal)
+	email := keyVal["email"]
+	pass := keyVal["pass"]
+	_, err = stmt.Exec(email, pass)
+	if err != nil {
+	  panic(err.Error())
+	}
+	fmt.Fprintf(w, "New user was created")
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request){
+	log.Print("reading records from database")
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil{
+		log.Print("error occurred while executing select query :: ",err)
+		return
+	}
+	users := []User{}
+	for rows.Next(){
+		var uid int
+		var email string
+		var pass string
+		err = rows.Scan(&uid, &email, &pass)
+		user := User{Uid: uid, Email: email, Pass: pass}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	log.Print("reading a record from database")
+	result, err := db.Query("SELECT * FROM users WHERE uid = ?", params["uid"])
+	if err != nil {
+	  panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+	  err := result.Scan(&user.Uid, &user.Email, &user.Pass)
+	  if err != nil {
+		panic(err.Error())
+	  }
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+
+func main(){
+	router := mux.NewRouter()
+	AddAuthHandlers(router)
+
+	//router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{Id}", getBook).Methods("GET")
+	router.HandleFunc("/books", createBook).Methods("POST")
+	router.HandleFunc("/books/{Id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{Id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/user/books/{uid}", Middleware(getUsBooks)).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users/{uid}", getUser).Methods("GET")
+	router.HandleFunc("/users/{uid}",deleteUser).Methods("DELETE")
+	router.HandleFunc("/books", Middleware(getBooks)).Methods("GET")
+	//router.HandleFunc("/token", LoginHandler).Methods("POST")
+
+	defer db.Close()
+	err := http.ListenAndServe(":"+CONN_PORT, router)
+	if err != nil{
+		log.Fatal("error starting http server :: ", err)
+		return
+	}
+} 
